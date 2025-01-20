533d22c9c3b51778544c2fec6d9a9a72cd9e448d
MrHB212 <h.barut97@gmail.com>
2025-01-20 20:59:06 +0100

    Verbinde mit WLAN, Kontrolleuchte aus wenn erfolgreich

---
+++ ESP8266_Zeitsteuerung/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+
+
+void setup() {
+  //LED Kontrolleuchte
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+  
+  //Wlan Einstellungen
+  const char* ssid ="KabelBox-2BF8";
+  const char* pw ="82644147";
+  WiFi.begin(ssid,pw);
+
+  
+}
+
+void loop() {
+  if(WiFi.status() == WL_CONNECTED){
+    digitalWrite(5, LOW);
+  }
+  
+}
+  
+
