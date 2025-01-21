cdcff3ec3e6fc343d6f927cfe2155ae3b7b751de
MrHB212 <h.barut97@gmail.com>
2025-01-21 20:37:58 +0100

    Versucht HTML und ESP zu verbinden
    HTML Skript geschrieben und auf dem ESP den code für den Server. alerdings scheint die json datei entweder nicht abgeschickt zu werden oder nicht anzukommen.

--- ESP8266_Zeitsteuerung/src/main.cpp
+++ ESP8266_Zeitsteuerung/src/main.cpp
@@ -1,25 +1,55 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
 
+//starte server
+ESP8266WebServer server(80);
+
+//Definiere konstante Einstellungen
+IPAddress local_ip(192,168,0,120);
+IPAddress gateway(192,168,0,1);
+IPAddress subnet(255,255,255,0);
+const char* ssid ="KabelBox-2BF8";
+const char* pw ="82644147";
+long long number = 99;
+
+
+
+//Empfange die Datei
+void handleInput(){
+  DynamicJsonDocument doc(76);
+  number = doc.as<long long>();
+  if(number == 99){
+    server.send(400, "text/plain", "FEHLER");
+  }else{
+    server.send(200, "text/plain", "OK");
+  }
+}
 
 
 void setup() {
-  //LED Kontrolleuchte
-  pinMode(5, OUTPUT);
-  digitalWrite(5, HIGH);
+//LED Kontrolleuchte
+pinMode(5, OUTPUT);
+digitalWrite(5, HIGH);
 
-  //Wlan Einstellungen
-  const char* ssid ="KabelBox-2BF8";
-  const char* pw ="82644147";
-  WiFi.begin(ssid,pw);
-
-  
+//mit WLAN verbinden
+WiFi.begin(ssid,pw);
+  if(WiFi.status() == WL_CONNECTED && WiFi.config(local_ip, gateway, subnet)){
+    digitalWrite(5, LOW);
+  }
+  //Starte Server
+  server.begin();
 }
 
 void loop() {
-  if(WiFi.status() == WL_CONNECTED){
-    digitalWrite(5, LOW);
+  server.on("/input", HTTP_POST, handleInput);
+  if(number==99){
+    digitalWrite(5, HIGH);
+  }else{
+    digitalWrite(5,LOW);
   }
+  
   
 }
   
