525014efaccb6c8b219d71164dfaf5a82c96b3a8
MrHB212 <h.barut97@gmail.com>
2025-06-30 16:47:34 +0200

    UPDATE

--- POST_GET.py
+++ POST_GET.py
@@ -2,6 +2,12 @@
 # Potenzieller Ersatz für HTML Variante ?
 
 import requests
-
-requests.post("HTTP://192.168.0.77/comms", json = "15501600")
+montag = "18001900"
+dienstag = "18051905"
+mittwoch = "18101910"
+donnerstag = "18151915"
+freitag = "18201920"
+samstag = "18251925"
+sonntag = "18301930"
+requests.post("HTTP://192.168.0.77/comms", json = montag+dienstag+mittwoch+donnerstag+freitag+samstag+sonntag)
 print("POST gesendet")
